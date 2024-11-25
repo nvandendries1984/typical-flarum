80cf8906feff0726916eb1debf5eeb56c1041182
nvandendries1984 <niels@nvandendries.nl>
2024-11-25 20:11:00 +0100

    Links Github added

--- extend.php
+++ extend.php
@@ -1,11 +1,6 @@
 <?php
 
-/*
- * This file is part of Flarum.
- *
- * For detailed copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
+namespace NielsVanDendries\TypicalFlarum;
 
 use Flarum\Extend;
 
